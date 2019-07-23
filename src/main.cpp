837a420da1b9045786ec7f1a78921184bc8b83cb
taunoe <sedumacre@gmail.com>
2019-07-23 21:48:11 +0300

    cleaner code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-/* 
- * Töötab ,testitud 20.07.219
+/*************************************************************
+ * Töötab ,testitud 23.07.219
  * ESP8266 
  * OLED SSD1306 128*64
  * 
@@ -8,7 +8,8 @@
  * D5-GPIO14 = SCLK
  * D7-GPIO13 = MOSI
  * D8-GPIO15 = CS
- */
+ * 
+ **************************************************************/
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -18,50 +19,48 @@
 
 #define SPEAKER_PIN          16
 
-#define SCREEN_I2C_ADDRESS 0x3c // use i2c scanner to find address 0x3c
-#define SCREEN_WIDTH        128 // OLED display width, in pixels
-#define SCREEN_HEIGHT        64 // OLED display height, in pixels
-#define SCREEN_RESET_PIN     10 // 16 ? not conected D0-GPIO16
+#define SCREEN_I2C_ADDRESS 0x3c  // use i2c scanner to find address 0x3c
+#define SCREEN_WIDTH        128  // OLED display width, in pixels
+#define SCREEN_HEIGHT        64  // OLED display height, in pixels
+#define SCREEN_RESET_PIN     10  // 16 ? not conected D0-GPIO16
 
-/* Buttons */
-#define BTN_UP           0  // D3
-#define BTN_SELECT       2  // D4
-#define BTN_DOWN        12  // D6
+#define BTN_UP                0  // D3
+#define BTN_SELECT            2  // D4
+#define BTN_DOWN             12  // D6
 
-#define MENU_FONT_SIZE   2
-#define MENU_ROW_HEIGHT 16
+#define MENU_FONT_SIZE        2
+#define MENU_ROW_HEIGHT      16
 
-/* Menu items */
-#define MENU_1_LENGTH    4
-const char * menu_1_items[] = {"Page 2", "Page 3", "Sound", "Exit"};
+#define MENU_1_LENGTH         4
+#define MENU_4_LENGTH         3
 
-#define MENU_4_LENGTH    3
-const char * menu_4_items[] = {"Buttons", "Light", "Exit"};
-uint8_t  menu_4_states[] = {0 , 0, 1};
+const char *menu_1_items[] = {"Page 2", "Page 3", "Sound", "Exit"};
+const char *menu_4_items[] = {"Buttons", "Light", "Exit"};
+uint8_t  menu_4_states[] = {1 , 0, 0};
 
 /* Global variables */
 uint8_t menu_pos {0};
 uint8_t page {0};
 
-int notes[] = {
-  NOTE_A4, NOTE_B4, NOTE_C3
-};
 
-/* Initialize screen */
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET_PIN);
-
-/**********************************
- * Custom function declarations
- **********************************/
-bool is_button(int pin);
+/************************************
+ *** Custom function declarations ***
+ ************************************/
+bool is_button(uint8_t pin);
 void move_menu_up (uint8_t menu_length);
 void move_menu_down (uint8_t menu_length);
+void play_note (uint8_t speaker_pin, uint16_t the_tone, uint8_t duration);
+void sound_on_off();
+void go_page(uint8_t new_page);
 void display_home ();
 void display_page_2 ();
 void display_page_3 ();
 void display_page_4 ();
 void display_menu_page (uint8_t menu_length, const char *menu[]);
 
+
+/* Initialize screen */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET_PIN);
 
 /********************************************************/
 void setup() {
@@ -74,11 +73,9 @@ void setup() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDRESS)) { // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // loop forever
-    
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
+  // Adafruit splash screen.
   display.clearDisplay();
   display.display();
   delay(1000);
@@ -86,76 +83,66 @@ void setup() {
 
 void loop() {
   if (page == 0) {
-    display_home ();
+      display_home ();
 
-    if (is_button(BTN_DOWN)) { 
-      // Go to menu 1
-      page = 1;
-      menu_pos = 0; 
-    }
-    
+      if (is_button(BTN_DOWN)) {   // Go to menu 1
+          go_page(1); 
+      }
   }
   else if (page == 1) {
-    display_menu_page (MENU_1_LENGTH, menu_1_items);
+      display_menu_page (MENU_1_LENGTH, menu_1_items);
     
-    if (is_button(BTN_DOWN)) {
-      move_menu_down(MENU_1_LENGTH);
-    }
-    else if (is_button(BTN_UP)) {
-      move_menu_up(MENU_1_LENGTH);
-    }
-    else if (is_button(BTN_SELECT)) {
-      if (menu_pos == 0) {
-        page = 2;
-        menu_pos = 0;
+      if (is_button(BTN_DOWN)) {
+          move_menu_down(MENU_1_LENGTH);
       }
-      else if (menu_pos == 1) {
-        page = 3;
-        menu_pos = 0;
+      else if (is_button(BTN_UP)) {
+          move_menu_up(MENU_1_LENGTH);
       }
-      else if (menu_pos == 2) {
-        page = 4;
-        menu_pos = 0;
+      else if (is_button(BTN_SELECT)) {
+          if (menu_pos == 0) {
+              go_page(2);
+          }
+          else if (menu_pos == 1) {
+              go_page(3);
+          }
+          else if (menu_pos == 2) {
+              go_page(4);
+          }
+          else if (menu_pos == 3) {
+              go_page(0);
+          }
       }
-      else if (menu_pos == 3) { // Exit
-        page = 0;               // Home page
-        menu_pos = 0;
-      }
-    }
-
   }
   else if (page == 2) {
-    display_page_2 ();
-
+      display_page_2 ();
   }
   else if (page == 3) {
-    display_page_3 ();
-
+      display_page_3 ();
   }
   else if (page == 4) {
-    
-    if (is_button(BTN_DOWN)) {
-      move_menu_down(MENU_4_LENGTH);
-    }
-    else if (is_button(BTN_UP)) {
-      move_menu_up(MENU_4_LENGTH);
-    }
-    else if (is_button(BTN_SELECT)) {
-      if (menu_4_states[menu_pos]) {
-        menu_4_states[menu_pos] = 0;
+      display_page_4();                     // sound page
+      if (is_button(BTN_DOWN)) {
+          move_menu_down(MENU_4_LENGTH);
       }
-      else {
-        menu_4_states[menu_pos] = 1;
+      else if (is_button(BTN_UP)) {
+          move_menu_up(MENU_4_LENGTH);
       }
-    }
-    display_page_4 (); // sound page
+      else if (is_button(BTN_SELECT)) {
+          if (menu_pos == 2) {
+              go_page(0);
+          }
+          if (menu_4_states[menu_pos]) {
+              menu_4_states[menu_pos] = 0;
+          }
+          else {
+              menu_4_states[menu_pos] = 1;
+          }
+      }
   }
   else { // page = 0 = home
-    display_home ();
+      display_home ();
   }
 
-
-  
 /*
   if (is_button(BTN_UP) && !is_button(BTN_SELECT) ){
     Serial.println("BNT_UP");
@@ -177,7 +164,9 @@ void loop() {
 } // loop end
 
 
-/* custom functions */
+/**************************
+ **** Custom Functions ****
+ **************************/
 
 /*!
  * @brief   Checks whether the button is pressed down or not.
@@ -185,7 +174,7 @@ void loop() {
  *          Button pin is pulledup (HIGH)
  * @return  True if is pressed down (LOW), else false
  */
-bool is_button(int pin) {
+bool is_button(uint8_t pin) {
   if (digitalRead(pin) == LOW) {
     delay(30);
 
@@ -198,6 +187,7 @@ bool is_button(int pin) {
   return false;
 }
 
+
 void move_menu_up (uint8_t menu_length) {
   if (menu_pos == 0) {
     menu_pos = menu_length - 1;
@@ -205,17 +195,41 @@ void move_menu_up (uint8_t menu_length) {
   else {
     menu_pos--;
   }
-  tone(SPEAKER_PIN, notes[1], 20);
+
+  sound_on_off();
 }
+
 
 void move_menu_down (uint8_t menu_length) {
   menu_pos++;
   if (menu_pos > menu_length - 1) {
     menu_pos = 0;
   }
-  tone(SPEAKER_PIN, notes[1], 20);
+
+  sound_on_off();
 }
 
+/*!
+ * @brief   Play sound if sate is 1
+ */
+void sound_on_off() {
+  if (menu_4_states[0] > 0) {
+    play_note(SPEAKER_PIN, NOTE_F7, 4);
+  }
+}
+
+/*!
+ * @brief   Play note
+ * @param   speaker_pin
+ * @param   the_tone
+ *          define on pitches.h
+ * @param   duration
+ *          1 to 7
+ */
+void play_note (uint8_t speaker_pin, uint16_t the_tone, uint8_t duration) {
+  long time = 500 / pow(2, duration - 1);
+  tone(speaker_pin, the_tone, time);
+}
 
 /*!
  * @brief   Display menu content
@@ -309,4 +323,14 @@ void display_page_4 () {
   }
 
   display.display();
+}
+
+
+/*!
+ * @brief   Change page nr and set menu position 0
+ * @note    new_page
+ */
+void go_page(uint8_t new_page) {
+  page = new_page;
+  menu_pos = 0;
 }
